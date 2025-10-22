a657b9952d0d5bc18809a03c17480e5ca687d430
Robert <robertjamesherold@icloud.com>
2025-10-22 20:31:28 +0200

    Push

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import { fileURLToPath } from "url";
